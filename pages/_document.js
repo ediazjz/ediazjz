c76591e03ddb970c54d064e838409a5dae699106
ediazjz <ediazjz@outlook.com>
2020-12-19 18:21:00 -0600

    Add Google Fonts

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Yantramanav:wght@500;700&family=Montserrat+Alternates:wght@400;600&family=Martel+Sans:wght@200;400;600&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
