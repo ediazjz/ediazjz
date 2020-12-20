bdb3fbb771c741631fa18debbfe9487f730134d5
ediazjz <ediazjz@outlook.com>
2020-12-19 19:34:16 -0600

    Add FontAwesome Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/globals.css'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+config.autoAddCss = false 
+
+// Add icons and packages you will use in multiple components
+library.add(fab)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <FontAwesomeIcon icon={["fab", "react"]} />
         </h1>
 
         <p className={styles.description}>
