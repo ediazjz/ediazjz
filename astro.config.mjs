e13aec07758bdae88ef9707067a74ca56fb60af9
ediazjz <iam.ediazjz@gmail.com>
2024-06-28 16:59:10 -0600

    build: add Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
+
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), svelte(), tailwind()]
+  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
